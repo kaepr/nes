5347d0c247680c2aa536cf9c9b2001198e588d89
shagun agrawal <agrawalshagun07@gmail.com>
2023-06-07 13:36:11 +0530

    refactor: shift instructions to separate namespace

--- src/nes/core.clj
+++ src/nes/core.clj
@@ -15,33 +15,8 @@
 (def test-cpu-state (cpu/create-cpu {:pc 0xC000 :cycles 7}))
 
 (defonce cartridge (cart/load-cartridge "../nes-test-roms/other/nestest.nes"))
-;(defonce cart-2 (cart/load-cartridge "../nes-test-roms/instr_test-v5/official_only.nes"))
 
 (cart/load-nestest (:memory test-cpu-state) cartridge)
-;(cart/load-rom (:memory test-cpu-state) cart-2)
-
-(defn log-state [state inst operands]
-  (let [pc (:pc state)
-        ^bytes memory (:memory test-cpu-state)
-        bytes-log (let [n-bytes (:bytes inst)]
-                    (->> (range 0 n-bytes)
-                         (map #(Byte/toUnsignedInt (aget memory (+ pc %))))
-                         (map #(format "%02X" %))
-                         (clojure.string/join " ")
-                         (#(str % (clojure.string/join " " (repeat 10 ""))))
-                         (#(subs % 0 9))))
-        cycles (:cycles state)
-        acc (Byte/toUnsignedInt (:reg-a state))
-        x-reg (Byte/toUnsignedInt (:reg-x state))
-        y-reg (Byte/toUnsignedInt (:reg-y state))
-        status-reg (Byte/toUnsignedInt (cpu/status-to-byte state))
-        sp (:sp state)]
-    (format "%04X  %s %s     A:%02X X:%02X Y:%02X P:%02X SP:%02X CYC:%d\n" pc bytes-log (name (:name inst)) acc x-reg y-reg status-reg sp cycles)))
-
-(defn log-nestest [^bytes memory]
-  (let [val1 (Byte/toUnsignedInt (aget memory 0x0002))
-        val2 (Byte/toUnsignedInt (aget memory 0x0003))]
-    (format "%02X %02X\n" val1 val2)))
 
 (defn run [init-state]
   (with-open [^Writer w (clojure.java.io/writer "log.txt")]
@@ -49,8 +24,8 @@
       (let [opcode (cpu/fetch state)
             decoded-inst (cpu/decode opcode)
             inst (cpu/fetch-operands state decoded-inst)
-            operands (cpu/handle-addressing-mode state inst)]
-        (.write ^Writer w ^String (log-nestest (:memory state))))
+            operands (cpu/handle-addressing-mode state inst)])
+        ;(.write ^Writer w ^String (log-nestest (:memory state))))
         ;(.write ^Writer w ^String (log-state state inst operands)))
       (recur (cpu/run-instruction state)))))
 
@@ -71,6 +46,3 @@
                     :alignment :center
                     :children [{:fx/type :label
                                 :text "Hello world"}]}}})))
-
-
-
--- src/nes/utils.clj
+++ src/nes/utils.clj
@@ -1,5 +1,18 @@
 (ns nes.utils)
 
+(def status-bit-vec [:carry :zero :interrupt-disable :decimal-mode :break-command :unused :overflow :negative])
+
+(defn status-to-byte [state]
+  (let [coll (map-indexed (fn [idx itm]
+                            (bit-shift-left (bool-to-int (itm state)) idx))
+                          status-bit-vec)
+        res (reduce bit-or coll)]
+    (unchecked-byte res)))
+
+(defn byte-to-status [status-byte]
+  (let [coll (map (fn [idx]
+                    {(nth status-bit-vec idx) (bit-test status-byte idx)}) (range 0 8))]
+    (reduce merge {} coll)))
 (defn bool-to-int [test]
   (if test 1 0))
 
@@ -12,6 +25,29 @@
   (if test
     (bit-set val pos)
     (bit-clear val pos)))
+
+(defn log-state [state inst _operands]
+  (let [pc (:pc state)
+        ^bytes memory (:memory state)
+        bytes-log (let [n-bytes (:bytes inst)]
+                    (->> (range 0 n-bytes)
+                         (map #(Byte/toUnsignedInt (aget memory (+ pc %))))
+                         (map #(format "%02X" %))
+                         (clojure.string/join " ")
+                         (#(str % (clojure.string/join " " (repeat 10 ""))))
+                         (#(subs % 0 9))))
+        cycles (:cycles state)
+        acc (Byte/toUnsignedInt (:reg-a state))
+        x-reg (Byte/toUnsignedInt (:reg-x state))
+        y-reg (Byte/toUnsignedInt (:reg-y state))
+        status-reg (Byte/toUnsignedInt (status-to-byte state))
+        sp (:sp state)]
+    (format "%04X  %s %s     A:%02X X:%02X Y:%02X P:%02X SP:%02X CYC:%d\n" pc bytes-log (name (:name inst)) acc x-reg y-reg status-reg sp cycles)))
+
+(defn log-nestest [^bytes memory]
+  (let [val1 (Byte/toUnsignedInt (aget memory 0x0002))
+        val2 (Byte/toUnsignedInt (aget memory 0x0003))]
+    (format "%02x %02x\n" val1 val2)))
 
 (defn word-lsb [val]
   (unchecked-byte (bit-and val 0xFF)))
